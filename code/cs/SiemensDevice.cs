71453f297c5126a5c6d237c202d4a8816c10e697
Dominik Scharnagl <48177425+dscharnagl@users.noreply.github.com>
2020-07-27 14:08:43 +0200

    Update SiemensDevice.cs

--- code/cs/SiemensDevice.cs
+++ code/cs/SiemensDevice.cs
@@ -2,3 +2,9 @@
 var device = new SiemensDevice("192.168.0.80");
 device.Type = SiemensDeviceType.S71200;
 device.ChannelType = SiemensChannelType.ProgrammerDevice;
+
+// DOC
+var device = new SiemensDevice();
+device.Type = SiemensDeviceType.S71500;
+device.ChannelType = SiemensChannelType.OperationPanel;
+device.EndPoint = new IPDeviceEndPoint("192.168.0.80", rack: 0, slot: 2);
