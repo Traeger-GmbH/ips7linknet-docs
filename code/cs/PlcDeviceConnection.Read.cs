44d5736f5c6a4ed2966df71678baa4dc2cf38e77
Dominik Scharnagl <48177425+dscharnagl@users.noreply.github.com>
2020-07-27 11:33:14 +0200

    Update PlcDeviceConnection.Read.cs

--- code/cs/PlcDeviceConnection.Read.cs
+++ code/cs/PlcDeviceConnection.Read.cs
@@ -13,12 +13,6 @@ var coordinatesVariable = new PlcInt32Array("DB1.DBD 1", 3);
 var coordinates = connection.ReadValue(coordinatesVariable);
 
 // DOC
-var speedVariable = new PlcInt32("DB1.DBD 1");
-var coordinatesVariable = new PlcInt32Array("DB2.DBD 1", 3);
-var jobIsActiveVariable = new PlcBoolean("DB3.DBX 1.0");
-var toolSetupVariable = new PlcBooleanArray("DB4.DBX 1.0", 5);
-var operatorNameVariable = new PlcString("DB5.DBB 1", 32);
-
 connection.ReadValues(
         speedVariable,
         coordinatesVariable,
