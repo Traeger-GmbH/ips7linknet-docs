a0f98bf3e970057c1354bd587715d6f17ae5d5d5
Dominik Scharnagl <48177425+dscharnagl@users.noreply.github.com>
2020-07-27 10:48:27 +0200

    Update PlcDeviceConnection.Read.cs

--- code/cs/PlcDeviceConnection.Read.cs
+++ code/cs/PlcDeviceConnection.Read.cs
@@ -3,3 +3,11 @@ int value = connection.ReadInt32("DB1.DBD 1");
 
 // DOC
 int[] values = connection.ReadInt32("DB1.DBD 1", 3);
+
+// DOC
+var speedVariable = new PlcInt32("DB1.DBD 1");
+var speed = connection.ReadValue(speedVariable);
+
+// DOC
+var coordinatesVariable = new PlcInt32("DB1.DBD 1", 3);
+var coordinates = connection.ReadValue(coordinatesVariable);
