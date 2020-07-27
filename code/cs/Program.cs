a461f53079991918d2163f0d5cf31ace7e5f2a74
Dominik Scharnagl <48177425+dscharnagl@users.noreply.github.com>
2020-07-27 09:51:46 +0200

    Create Program.cs

---
+++ code/cs/Program.cs
@@ -0,0 +1,21 @@
+// DOC
+using IPS7Lnk.Advanced;
+
+// DOC
+var device = new SiemensDevice("192.168.0.80");
+
+// DOC
+var connection = device.CreateConnection();
+connection.Open();
+
+// DOC
+// Your code to interact with the controller.
+
+// DOC
+connection.Close();
+
+// DOC
+using (var connection = device.CreateConnection()) {
+    connection.Connect();
+    // Your code to interact with the controller.
+}
